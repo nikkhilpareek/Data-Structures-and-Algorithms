4e7d7a42776e1bb5278ed9145e605bcd827e9ee2
Nikhil Pareek <114740943+nikkhilpareek@users.noreply.github.com>
2023-09-26 19:14:10 +0530

    Insertion Sort - Coding Ninjas

---
+++ Day 1/Insertion_Sort.cpp
@@ -0,0 +1,11 @@
+void insertionSort(int n, vector<int> &arr){
+    for(int i = 0; i<n; i++){
+        int j = i;
+        while(j>0 && arr[j-1]>arr[j]){
+            int temp = arr[j-1];
+            arr[j-1] = arr[j];
+            arr[j] = temp;
+            j--;
+        }
+    }
+}
