e400b7b9c340e6ea0bcfef974f8c421ada996353
Nikhil Pareek <114740943+nikkhilpareek@users.noreply.github.com>
2023-09-29 13:53:44 +0530

    Left Rotate Array by one Place

--- Day 4/Left_Rotate_Array_by_one_place.cpp
+++ Day 4/Left_Rotate_Array_by_one_place.cpp
@@ -1,6 +1,20 @@
+// Using C++ STL
+
 #include <bits/stdc++.h> 
 vector<int> rotateArray(vector<int>& arr, int n) {
     reverse(arr.begin(), arr.end());
     reverse(arr.begin(),arr.end()-1);
     return arr;
+}
+
+// Using For Loop
+#include <bits/stdc++.h> 
+vector<int> rotateArray(vector<int>& arr, int n) {
+    int temp = arr[0];
+    for(int i =1; i<n; i++){
+        arr[i-1] = arr[i];
+    }
+    arr[n-1] = temp;
+    return arr;
+
 }
