fc1f06d89b6f7aea5a8473aa5da9583fa87b8800
Nikhil Pareek <114740943+nikkhilpareek@users.noreply.github.com>
2023-09-30 20:02:45 +0530

    Maximum Consecutive Ones

---
+++ Day 5/Max_Consecutive_ones.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int maximum = 0;
+        int count = 0;
+        for(int i = 0; i<nums.size(); i++){
+            if(nums[i] == 1){
+                count++;
+                maximum = max(maximum,count);
+            }
+            else{
+                count = 0;
+            }
+        }
+        return maximum;
+    }
+};
