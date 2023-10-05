b8d57ae1ce92326e917c9efe4004a790234bbed7
Nikhil Pareek <114740943+nikkhilpareek@users.noreply.github.com>
2023-10-05 23:17:33 +0530

    Longest Subarray with Sum K - Only Positives

---
+++ Day 6/Longest_Subarray_with_sum_k_only_positive.cpp
@@ -0,0 +1,20 @@
+int longestSubarrayWithSumK(vector<int> a, long long k) {
+    int left = 0, right = 0;
+    int maxLen = 0;
+    int n = a.size();
+    long long sum = a[0];
+    while(right<n){
+        while(left<=right && sum >k){
+            sum -= a[left];
+            left++;
+        }
+        if(sum == k){
+            maxLen = max(maxLen, right-left+1);
+        }
+        right++;
+        if(right<n){
+            sum += a[right];
+        }
+    }
+    return maxLen;
+}
